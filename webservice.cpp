4f7ca0adb528940b881cc1bc88ba4ce1083de0a7
Ray Doetjes <rdoetjes@phonax.com>
2021-05-07 09:27:17 +0100

    get arguments for webservice

--- webservice.cpp
+++ webservice.cpp
@@ -1,7 +1,7 @@
 #include "webservice.hpp"
 
 
-const std::shared_ptr<http_response> webservice::render(const http_request&) {
-  hornio::hornDoubleTap(300, 600);
+const std::shared_ptr<http_response> webservice::render(const http_request& req) {
+  hornio::hornDoubleTap( stoi(req.get_arg("msShort")), stoi(req.get_arg("msLong")) );
   return std::shared_ptr<http_response>(new string_response("HONK, HONK!"));
 }
